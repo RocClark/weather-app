bd60764179e494f2ab2bd72835523293e8f0f4a4
Roscoe Clark <roscoe.clark9@gmail.com>
2024-09-20 15:33:05 -0400

    setup

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"B84a0pW82e982KG92ttX+N+ktFc5iprkfFjzUX31I5o=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BgYuSBDrI1/OGu0pR/gCj73X9T/h+LY5aCxQLTd+g/k=\"\n}"
